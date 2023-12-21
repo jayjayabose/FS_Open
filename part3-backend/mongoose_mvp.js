e8f43f0381cce5ac0834f8ec15c6a5a65bd9b8ca
Sundar Jayabose <jayabose@gmail.com>
2023-12-21 17:28:26 -0500

    mongoose mvp

---
+++ part3-backend/mongoose_mvp.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+
+const password = 'FJNdhMdwpDlfSi8t' // not the atlas password. db cluster password? seems if you lose it, create a new user
+const url = `mongodb+srv://jayabose:${password}@cluster0.4k9meiq.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.set('strictQuery',false);
+
+mongoose.connect(url);
+console.log('connect');
+
+// https://mongoosejs.com/docs/index.html
+// schema will be used to create a modle
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+});
+
+// model is used to create documents
+const Note = mongoose.model('Note', noteSchema);
+
+// create a document
+const note = new Note({
+  content: 'HTML is Easy - foo',
+  important: true,
+})
+
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+});
